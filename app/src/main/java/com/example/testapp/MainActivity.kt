f61de63800945668c29910dc40054a22e2f5ad85
Timur Takhtashov <Timurtakhtashov2006@gmail.com>
2024-05-20 23:27:23 +0200

    all bags fixed

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -5,17 +5,29 @@ import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.view.MotionEvent
 import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.example.testapp.Constance.ANIMATION_DURATION
+import com.example.testapp.Constance.SLEEP_HOLDER_DURATION
 import com.example.testapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bindingClass: ActivityMainBinding
-    private var animatorSet: AnimatorSet? = null
-    private val handler = android.os.Handler()
+    private var animationUpAndDownFromFirstPosition: AnimatorSet? = null
+    private var animationUpAndDown: AnimatorSet? = null
+    private val sleepHandler = android.os.Handler(Looper.getMainLooper())
+    private var isSleepHandlerRunning = false
+    private var animationCancelled = false
+
 
     @SuppressLint("ClickableViewAccessibility", "Recycle")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,54 +35,114 @@ class MainActivity : AppCompatActivity() {
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
-        bindingClass.tvGreeting.setTextColor(resources.getColor(R.color.greeting_text_color))
+        hideSystemUI()
+        bindingClass.tvGreeting.text = getString(R.string.Hello).uppercase()
 
         bindingClass.background.setOnTouchListener { _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
+
+                    bindingClass.tvGreeting.setTextColor(ContextCompat.getColor(this, R.color.greeting_text_color))
+                    stopAnimation(animationUpAndDownFromFirstPosition)
+                    stopAnimation(animationUpAndDown)
+
+                    if (!isSleepHandlerRunning) {
+                        isSleepHandlerRunning = true
+                        sleepHandler.postDelayed({
+                            textAnimation()
+                            isSleepHandlerRunning = false
+                        }, SLEEP_HOLDER_DURATION)
+                    } else {
+                        stopAnimation(animationUpAndDownFromFirstPosition)
+                        stopAnimation(animationUpAndDown)
+                        sleepHandler.removeCallbacksAndMessages(null)
+                        sleepHandler.postDelayed({
+                            textAnimation()
+                        }, SLEEP_HOLDER_DURATION)
+                    }
+
                     val x = event.x
                     val y = event.y
                     bindingClass.tvGreeting.x = x
                     bindingClass.tvGreeting.y = y
-                    bindingClass.tvGreeting.visibility = View.VISIBLE
-
-                    handler.postDelayed({
-                        startTextAnimation()
-                    }, 5000)
                 }
             }
             true
         }
     }
 
-    private fun startTextAnimation() {
-        animatorSet?.cancel()
-        textAnimation()
+    fun stopAnimationOnTextClick(view: View){
+        stopAnimation(animationUpAndDown)
+        stopAnimation(animationUpAndDownFromFirstPosition)
     }
 
     private fun textAnimation() {
-        val screenHeight = bindingClass.background.height.toFloat()
-        val currentTranslationY = bindingClass.tvGreeting.translationY
+        bindingClass.apply {
+            val params = tvGreeting.layoutParams as ConstraintLayout.LayoutParams
+            val verticalBias = params.verticalBias
 
-        val animationDown = ObjectAnimator.ofFloat(bindingClass.tvGreeting, "translationY", currentTranslationY, screenHeight)
-        animationDown.duration = 3000
+            val currentTranslationY = tvGreeting.translationY
+            val bottomOfScreen = background.height-tvGreeting.height/2-verticalBias*background.height+tvGreeting.textSize
+            val topOfScreen = -verticalBias*background.height+tvGreeting.height/2-tvGreeting.textSize
 
-        val animationFromBottomToTop = ObjectAnimator.ofFloat(bindingClass.tvGreeting, "translationY", screenHeight, 0f)
-        animationFromBottomToTop.duration = 3000
+            val animationDown = ObjectAnimator.ofFloat(tvGreeting, "translationY", currentTranslationY, bottomOfScreen)
+            animationDown.duration = ANIMATION_DURATION
 
-        val animationFromTopToBottom = ObjectAnimator.ofFloat(bindingClass.tvGreeting, "translationY", 0f, screenHeight)
-        animationFromTopToBottom.duration = 3000
+            val animationFromBottomToTop = ObjectAnimator.ofFloat(tvGreeting, "translationY", bottomOfScreen, topOfScreen)
+            animationFromBottomToTop.duration = ANIMATION_DURATION
 
-        animatorSet = AnimatorSet()
-        animatorSet?.play(animationDown)?.before(animationFromBottomToTop)
-        animatorSet?.play(animationFromBottomToTop)?.before(animationFromTopToBottom)
+            val animationFromTopToBottom = ObjectAnimator.ofFloat(tvGreeting, "translationY", topOfScreen, bottomOfScreen)
+            animationFromTopToBottom.duration = ANIMATION_DURATION
 
-        animatorSet?.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationEnd(animation: Animator) {
-                textAnimation()
+
+            animationUpAndDownFromFirstPosition = AnimatorSet()
+            animationUpAndDownFromFirstPosition?.play(animationDown)?.before(animationFromBottomToTop)
+            animationUpAndDownFromFirstPosition?.play(animationFromBottomToTop)?.before(animationFromTopToBottom)
+
+            animationUpAndDown = AnimatorSet()
+            animationUpAndDown?.play(animationFromBottomToTop)?.before(animationFromTopToBottom)
+
+            val listenerForUpAndDownAnimationCancel = object : AnimatorListenerAdapter() {
+                override fun onAnimationCancel(animation: Animator) {
+                    animationCancelled = true
+
+                }
             }
-        })
 
-        animatorSet?.start()
+            val listenerForUpAndDownAnimation = object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    if (!animationCancelled) {
+                        animationUpAndDown?.start()
+                    }
+                    animationCancelled=false
+                }
+            }
+
+            animationUpAndDownFromFirstPosition?.addListener(listenerForUpAndDownAnimation)
+            animationUpAndDown?.addListener(listenerForUpAndDownAnimation)
+            animationUpAndDownFromFirstPosition?.addListener(listenerForUpAndDownAnimationCancel)
+            animationUpAndDown?.addListener(listenerForUpAndDownAnimationCancel)
+
+            animationUpAndDownFromFirstPosition?.start()
+        }
     }
+
+    private fun stopAnimation (animatorSet: AnimatorSet?){
+        if (animatorSet?.isStarted == true) {
+            animatorSet.cancel()
+        }
+    }
+
+    private fun hideSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.let { controller ->
+                controller.hide(WindowInsets.Type.systemBars())
+                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        }
+    }
+}
+object Constance {
+    const val ANIMATION_DURATION: Long = 1000
+    const val SLEEP_HOLDER_DURATION: Long = 2000
 }
